f66c6cf94744d22f695173cc616b4b105f4095b0
Ratul8863 <ratulroy8863@gmail.com>
2025-06-17 18:07:27 +0600

    firebase .env

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDRIb5qQ7b-ig_vkIJFs8w2eDNf6QdfQnw",
-  authDomain: "product-recommendation-byratul.firebaseapp.com",
-  projectId: "product-recommendation-byratul",
-  storageBucket: "product-recommendation-byratul.firebasestorage.app",
-  messagingSenderId: "704958507888",
-  appId: "1:704958507888:web:fd3c10742913b3755cfdb5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
